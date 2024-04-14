5e0f1fa49aea19f5557cc5415827df1966fe6564
Yuya Isaka <zxwg6329@gmail.com>
2024-04-14 22:56:40 +0900

    add comment

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -3,6 +3,7 @@ package evaluator
 import "github.com/yuya-isaka/go-yuya-monkey/object"
 
 var builtins = map[string]*object.BuiltinObj{
+	// 現状ただのラッパー
 	"len": &object.BuiltinObj{
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
