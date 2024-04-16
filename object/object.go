db1719ed0d0e55c81da09a09f1436067d12bcb3e
Yuya Isaka <zxwg6329@gmail.com>
2024-04-16 22:29:48 +0900

    add hashmap object

--- object/object.go
+++ object/object.go
@@ -3,6 +3,7 @@ package object
 import (
 	"bytes"
 	"fmt"
+	"hash/fnv"
 	"strings"
 
 	"github.com/yuya-isaka/go-yuya-monkey/ast"
@@ -148,4 +149,32 @@ func (a ArrayObj) Inspect() string {
 	out.WriteString("]")
 
 	return out.String()
+}
+
+type HashKey struct {
+	Type  ObjectType // BoolかIntかString
+	Value uint64
+}
+
+func (b *BoolObj) HashKey() HashKey {
+	var value uint64
+
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{Type: b.Type(), Value: value}
+}
+
+func (i *IntObj) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *StringObj) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
 }
